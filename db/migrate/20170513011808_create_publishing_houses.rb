ae03e31a7639979cfd2d9ecc351bd4d1546726f3
Michael Ogala <michael.ogala@andela.com>
2017-05-13 02:19:28 +0100

    setup book, publishing_house and author models

---
+++ db/migrate/20170513011808_create_publishing_houses.rb
@@ -0,0 +1,9 @@
+class CreatePublishingHouses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :publishing_houses do |t|
+      t.decimal :discount, precision: 2, scale: 2
+
+      t.timestamps
+    end
+  end
+end
